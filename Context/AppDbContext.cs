0b5ced798dd4739ab306810f6ab90fdb81400cf4
Davi Vieira <100722433+sdavivieira@users.noreply.github.com>
2023-11-12 22:49:05 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using AlunosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlunosAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Maria",
+                    Email = "mariadapenha@gmail.com",
+                    Idade = 23
+                },
+                 new Aluno
+                 {
+                     Id = 2,
+                     Nome = "Manuel",
+                     Email = "Manuelbueno@gmail.com",
+                     Idade = 23
+                 }
+                 );
+
+        }
+    }
+}
